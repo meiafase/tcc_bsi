4759b76f0f74f5daf07e727500982e9df3067fef
Ana Maria Vargas <aninha_vargas@hotmail.com>
2023-08-15 20:27:48 -0300

    rota login

---
+++ ticket-api-laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validação dos dados de entrada
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Criar novo usuário
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Gerar token para o usuário
+        $token = $user->createToken('token-name')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token-name')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        // dd($request);
+        $user = $request->user();
+
+        // Revogar todos os tokens do usuário
+        $user->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
