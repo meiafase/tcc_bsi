b475fa28d595eaaa5741ce2f3d021307ba395df0
Samuel Diel <samuelsdiel@gmail.com>
2023-08-29 20:59:09 -0300

    adicionando arquivo de rotas no client-side

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,24 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
-import OpenRequest from './pages/openRequest/Index';
-import MyService from './pages/myService/Index';
-import Report from './pages/Report/Index';
-import NewRequest from './pages/newRequest/Index';
-import Login from './pages/login/Index';
-import HeaderTeste from './pages/components/header/Index'
+import Rotas from './routes/Routes';
 
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/AbrirRequisicao" element={<OpenRequest />} />
-          <Route path="/MeusAtendimentos" element={<MyService />} />
-          <Route path="/Relatorio" element={<Report />} />
-          <Route path="/NovaSolicitacao" element={<NewRequest />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/header" element={<HeaderTeste />} />
-        </Routes>
+        <Rotas />
     </div>
   );
 }
